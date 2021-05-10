24e6a26ddba56fc6dd101c97be4576856738b432
ahmedalhindi <alhindi.ahmed@gmail.com>
2021-05-10 14:47:19 +0300

    bootstrap theme

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
+const path = require('path')
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
 
 // Declare a route
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
+fastify.register(require('fastify-static'), {
+    root: path.join(__dirname, 'public'),
+    prefix: '/public/', // optional: default '/'
+})
+
+fastify.get('/', function (req, reply) {
+    return reply.sendFile('index.html') // serving path.join(__dirname, 'public', 'myHtml.html') directly
 })
 
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3001)
+    await fastify.listen(3000)
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
